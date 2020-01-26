c40355ceeba376d5d107867ab5047f546db185d7
Mathieu Ridet <mathieuridet99@gmail.com>
2020-01-26 14:37:12 +0100

    26/01/2020

--- src/main.c
+++ src/main.c
@@ -20,6 +20,8 @@
 #define TAILLE_MAX_CHEMIN 50
 #define TAILLE_MAX_FICHIER 1024
 #define TAILLE_LIGNE 1000
+#define TAILLE_TUBES 2
+#define NB_PRODUCTEURS 2
 
 pthread_t thread1, thread2, thread3;
 
@@ -29,10 +31,10 @@ pthread_barrier_t barrier;
 //pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
 // Descripteurs de fichier (fd1 pour fichier 1 et fd2 pour fichier 2)
-int fd1, fd2;
+int fd1, fd2, sig;
 
 // Tube utilise pour la transmission de la valeur de resultat final
-int t_res[2];
+int t_res[TAILLE_TUBES];
 
 // Tampons contenant les contenus des fichiers
 char *tampon_fichier1[TAILLE_LIGNE], *tampon_fichier2[TAILLE_LIGNE];
@@ -46,29 +48,17 @@ static void cleanup_handler(void *arg){
 */
 
 void *producteur_routine(void* f) {
-	printf("(INFO-49) *** Producteur ***\n");
+	//printf("(INFO-49) *** Producteur ***\n");
 
 	pthread_t tid = pthread_self();
-	int returnBarrier;
-	char *tamponRoutine;
-	int fd;
-	//int numThread = *((int *) f);
-	char car[2];
-	int i;
+	//char tamponRoutine[TAILLE_LIGNE];
+	int fd, i, retour_barrier;
+	char c, *tamponRoutine;
 
-	printf("PROD : APRES VAR\n");
+	// TODO : remplacer la 1ere ligne du main par celle ci, si besoin
+	// On attend 2 threads (les producteurs) au point de rdv
+	//pthread_barrier_init(&barrier,NULL,NB_PRODUCTEURS);
 
-	/*
-	if (numThread == 1){
-		fd = fd1;
-		tamponRoutine = tampon_fichier1;
-	}else if(numThread == 2){
-		fd = fd2;
-		tamponRoutine = tampon_fichier2;
-	}else{
-		perror("(ERROR-P101) Numéro de thread inconnue");
-		exit(EXIT_FAILURE);
-	}*/
 	if (pthread_equal(thread2, tid)){
 		fd = fd1;
 		tamponRoutine = tampon_fichier1;
@@ -77,90 +67,98 @@ void *producteur_routine(void* f) {
 		tamponRoutine = tampon_fichier2;
 	}
 
-	printf("PROD : APRES CHOIX PROD 1 OU 2\n");
-
 	// Ensemble de signaux a considerer
 	sigset_t sigs_to_catch;
 	sigemptyset(&sigs_to_catch);
 	sigaddset(&sigs_to_catch, SIGUSR2);
 
 	while(1) {
-		printf("PROD : DEBUT WHILE(1)\n");
-
 		i = 0;
 		memset(tamponRoutine, 0, TAILLE_LIGNE);
-		while(car[0] != '\n' && car[0] != '\0'){
-			if (read(fd, car, 1) == -1){
+		do {
+			if (read(fd, &c, 1) == -1){
 				perror("(ERROR-P102) lecture d'une ligne KO");
 				exit(EXIT_FAILURE);
 			}
-			printf("PROD : DANS 2e WHILE\n");
-			
-			tamponRoutine[i] = car[0];
+			tamponRoutine[i] = c;
 			i++;
-		}
+		} while(c != '\n' && c != '\0');
+
+		//printf("PROD : AVANT BARRIERE\n");
 
 		// Si un thread arrive ici, il attend le 2e producteur avant de pouvoir continuer
-		if(pthread_barrier_wait(&barrier) != 0){
-			printf("PROD : ENVOI SIGNAL POUR CONTINUER\n");
-					
-			pthread_kill(&thread1, SIGUSR1);
+		retour_barrier = pthread_barrier_wait(&barrier);
+
+		//printf("PROD : APRES BARRIERE\n");
+
+		if(retour_barrier != 0 && retour_barrier != PTHREAD_BARRIER_SERIAL_THREAD) {
+			perror("pthread_barrier_wait");
+			exit(EXIT_FAILURE);
 		}
+		else if(retour_barrier == PTHREAD_BARRIER_SERIAL_THREAD){
+			// Un seul thread a cette valeur de retour, il envoit le signal au consommateur
+			pthread_kill(thread1, SIGUSR1);
+		}
+		
 		// Les producteurs ont fini de remplir les tampons avec une ligne chacun, on envoit un signal au consommateur
 		// On attend que le consommateur compare les lignes (reprennent quand reçoivent SIGUSR2);
-		printf("PROD : ATTENTE SIGNAL\n");
-		sigwait(&sigs_to_catch, NULL);
-		printf("PROD : SIGNAL RECU\n");
-
+		if(sigwait(&sigs_to_catch, &sig) != 0) {
+			perror("(ERROR) sigwait dans producteur");
+			exit(EXIT_FAILURE);
+		}
 	}
+
+	pthread_barrier_destroy(&barrier);
 
 }
 
 
 
 void *consommateur_routine(void *i) {
-	printf("(INFO-122) *** Consommateur ***\n");
+	//printf("(INFO-122) *** Consommateur ***\n");
 
-	// Si fichier identifique on retournera 0, sinon 1
-	int result = 0;
+	// Si fichier identifique on retournera 0, sinon num_ligne
+	int result = -1, num_ligne = 1;
 	// Ensemble de signaux a considerer
 	sigset_t sigs_to_catch;
 	sigemptyset(&sigs_to_catch);
 	sigaddset(&sigs_to_catch, SIGUSR1);
 
-	printf("CONSO : APRES VAR\n");
-
 	while(1) {
 		// On attend que les producteurs ecrivent leur ligne (reprend quand on recoit SIGUSR1)	
-		printf("CONSO : AVANT RECEPTION SIGNAL\n");
-		printf("CONSO : tampon_fichier1 = %s\n", *tampon_fichier1);
-		printf("CONSO : tampon_fichier2 = %s\n", *tampon_fichier2);
-		sigwait(&sigs_to_catch, NULL);
-		printf("CONSO : APRES RECEPTION SIGNAL\n");
-		printf("CONSO : tampon_fichier1 = %s\n", *tampon_fichier1);
-		printf("CONSO : tampon_fichier2 = %s\n", *tampon_fichier2);
+		if(sigwait(&sigs_to_catch, &sig) != 0) {
+			perror("(ERROR) sigwait dans consommateur");
+			exit(EXIT_FAILURE);
+		}
 
 		// Les 2 fichiers possedent encore des lignes, on les compare
-		if(strcmp(*tampon_fichier1, *tampon_fichier2)) {
+		if(strcmp(tampon_fichier1, "\n\0")!=0 && strcmp(tampon_fichier1, tampon_fichier2)==0) {
+			// On met result a 0 = lignes identiques
+			result = 0;			
+			if(tampon_fichier1[0]=='\0') {
+				// Les fichiers sont vides a la base
+				pthread_cancel(thread2);
+				pthread_cancel(thread3);
+				break;
+			}
+			num_ligne++;
 			// On envoie un signal aux producteurs pour les sortir de leur pause et passer a la ligne suivante
-			pthread_kill(&thread2, SIGUSR2);
-			pthread_kill(&thread3, SIGUSR2);
-		} else if(strcmp(*tampon_fichier1, "") && strcmp(*tampon_fichier2, "")) {
-			// Si les 2 fichiers sont vides, on s'arrete 
+			pthread_kill(thread2, SIGUSR2);
+			pthread_kill(thread3, SIGUSR2);
+		} else if(strcmp(tampon_fichier1, "\n\0") == 0 && strcmp(tampon_fichier2, "\n\0") == 0) {
+			// Il n'y a plus de lignes à lire dans aucun des 2 fichiers, on s'arrete 
 			// (les fichiers sont equivalents sinon on se serait deja arrete)
 			pthread_cancel(thread2);
 			pthread_cancel(thread3);
 			break;
-		} 
-		else {
+		} else {
 			// Lignes differentes
-			printf("CONSO : LIGNES DIFFERENTES\n");
-			result = 1;
 
 			// On stoppe les threads producteurs
 			pthread_cancel(thread2);
 			pthread_cancel(thread3);
-
+			// Le consommateur se termine en retournant le num de la ligne qui diffère
+			pthread_exit((void*) num_ligne);
 			break;
 		}
 	}
@@ -168,19 +166,20 @@ void *consommateur_routine(void *i) {
 	// TODO : gestionnaire de nettoyage
 
 
-	printf("CONSO : FIN\n");
-
 	// On stoppe le consommateur
-	pthread_exit(result);
+	pthread_exit((void*) result);
 }
 
 
 int main(int argc, char** argv) {
+	// Initialisation de la barriere des threads producteurs
+	pthread_barrier_init(&barrier,NULL,NB_PRODUCTEURS);
+
 	// pid1 = processus entrees/sorties, pid2 = processus de traitement
 	pid_t pid1, pid2;
 
 	// t1 = processus entrees/sorties, t2 = processus de traitement
-	int t1[2], t2[2];
+	int t1[TAILLE_TUBES], t2[TAILLE_TUBES];
 	pipe(t1);
 	pipe(t2);
 
@@ -191,6 +190,14 @@ int main(int argc, char** argv) {
 	// Sert de tampon pour les transmissions entre processus (la valeur de retour du consommateur)
 	char buf_res[2];
 
+	// On cree un masque de signaux pour le processus principal
+	// afin qu'il ne reagisse pas aux signaux SIGUSR1 & SIGUSR2
+	sigset_t sigs_to_ignore;
+	sigemptyset(&sigs_to_ignore);
+	sigaddset(&sigs_to_ignore, SIGUSR1);
+	sigaddset(&sigs_to_ignore, SIGUSR2);
+	sigaddset(&sigs_to_ignore, SIGSEGV);
+	sigprocmask(SIG_BLOCK, &sigs_to_ignore, NULL);
 
 	// On cree le processus de traitement
 	if((pid2 = fork()) == -1) {
@@ -224,23 +231,24 @@ int main(int argc, char** argv) {
 		}
 
 		// On crée les threads pour faire les traitements (dans les routines)
-		if(pthread_create(&thread1, NULL, consommateur_routine, 0) != 0) {
+		if(pthread_create(&thread1, NULL, consommateur_routine, NULL) != 0) {
 			printf("(ERROR-226) Creation thread 1 KO\n");
 			exit(EXIT_FAILURE);
-		} else if(pthread_create(&thread2, NULL, producteur_routine, 1) != 0) {
+		} else if(pthread_create(&thread2, NULL, producteur_routine, NULL) != 0) {
 			printf("(ERROR-229) Creation thread 2 KO\n");
 			exit(EXIT_FAILURE);
-		} else if(pthread_create(&thread3, NULL, producteur_routine, 2) != 0) {
+		} else if(pthread_create(&thread3, NULL, producteur_routine, NULL) != 0) {
 			printf("(ERROR-232) Creation thread 3 KO\n");
 			exit(EXIT_FAILURE);
 		}
 
 		// On recupere le resultat de la comparaison des fichiers :
-		// Si egaux status = 0, sinon status = 1
-		void *status = -1;
-		pthread_join(thread1, &status);
+		// Si egaux res_consommateur = 0, sinon res_consommateur = 1
+		int * res_consommateur;
+		pthread_join(thread1, (void**) &res_consommateur);
+
 		// On transmet le resultat au processus d'E/S
-		buf_res[0] = (int) status;
+		buf_res[0] = res_consommateur;
 		if(write(t_res[1], buf_res, sizeof(buf_res)) == -1) {
 			perror("(ERROR-245) Write res final KO");
 			exit(EXIT_FAILURE);
@@ -253,10 +261,6 @@ int main(int argc, char** argv) {
 		exit(EXIT_SUCCESS);
 	} else {
 		// ***** PROCESSUS PERE *****
-		/**
-		 * Il n'y a rien à faire ici.
-		 * On crée juste le processus d'E/S.
-		**/
 
 		if((pid1 = fork()) == -1) {
 			perror("(ERROR-261) Fork processus E/S KO");
@@ -285,18 +289,20 @@ int main(int argc, char** argv) {
 				exit(EXIT_FAILURE);
 			}
 
+			printf("\n");
+
 			// Lecture de la reponse du processus de traitement via le tube t_res
 			if(read(t_res[0], buf_res, sizeof(buf_res)) == -1) {
 				perror("(ERROR-288) Read retour du processus de traitement KO ");
 				exit(EXIT_FAILURE);
 			}
 			// Transmission resultat a l'utilisateur (sur la sortie standard)
-			if(buf_res[0] == 0) printf("(INFO) Les 2 fichiers sont identiques!\n");
-			else if(buf_res[0] == 1) printf("(INFO) Les fichiers sont différents!\n");
+			if(buf_res[0] == 0) printf("(SUCCESS) Les 2 fichiers sont identiques!\n");
 			else {
-				printf("(ERROR-296) Resultat KO : Le buffer contient une valeur de retour inconnue !\n");
+				printf("(FAILURE) Les fichiers sont différents!\n          La ligne %d diffère.\n", buf_res[0]);
 				exit(EXIT_FAILURE);
 			}
+			printf("\n");
 
 			// Processus d'entrees/sorties termine avec succes
 			close(t1[1]);
@@ -308,7 +314,18 @@ int main(int argc, char** argv) {
 		// On attend la fin du processus de traitement
 		int status = 100;
 		waitpid(pid2, &status, 0);
-		
+
+		// Utile pour verifier que le programme ne s'arrete pas par un signal
+		/*
+		if(WIFEXITED(status)) {
+			printf("WEXITSTATUS %d\n", WEXITSTATUS(status));
+		} else if(WIFSIGNALED(status)) {
+			printf("WTERMSIG %d\n", WTERMSIG(status));
+		} else if(WIFSTOPPED(status)) {
+			printf("WSTOPSIG %d\n", WSTOPSIG(status));
+		}
+		*/
+
 		// On appelle le gestionnaire de nettoyage pour libérer les ressources utilisées.
 		//pthread_cleanup_pop(1);
 
